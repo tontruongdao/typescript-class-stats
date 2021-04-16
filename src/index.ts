271a58d39a3122ecc3a7aff23d5d1290cb861830
Truong Dao <ton.truong.dao@outlook.com>
2021-04-16 11:28:55 -0400

    Counted # wins for ManU

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,21 @@ import fs from 'fs'
 
 const matches = fs.readFileSync('football.csv', {
     encoding: 'utf-8'
-})
-    .split('\n')
-    .map((row: string):string[] => {
-        return row.split(',')
     })
+.split('\n')
+.map((row: string):string[] => {
+    return row.split(',')
+})
 
-console.log(matches)
+
+let manUWins = 0;
+
+for (let match of matches) {
+    if (match[1] === 'Man United' && match[5] === 'H') {
+        manUWins++
+    } else if (match[2] === "Man United" && match[5] === "A") {
+        manUWins++
+    }
+}
+
+console.log(`Manchester United won ${manUWins} games`)
