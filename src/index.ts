c3d608aab8edb10c3a1bb165aa190448bf48d605
Truong Dao <ton.truong.dao@outlook.com>
2021-04-16 13:25:52 -0400

    Used TUPLE to declare a new type of data.

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,15 @@
 import { CsvFileReader } from './CsvFileReader'
+import { MatchResult } from './MatchResult'
 
 const reader = new CsvFileReader('football.csv')
 reader.read();
 
-enum matchResult {
-    homeWin = 'H',
-    awayWin = 'A',
-    Draw = 'D'
-}
-
 let manUWins = 0;
 
 for (let match of reader.data) {
-    if (match[1] === 'Man United' && match[5] === matchResult.homeWin) {
+    if (match[1] === 'Man United' && match[5] === MatchResult.homeWin) {
         manUWins++
-    } else if (match[2] === 'Man United' && match[5] === matchResult.awayWin) {
+    } else if (match[2] === 'Man United' && match[5] === MatchResult.awayWin) {
         manUWins++
     }
 }
