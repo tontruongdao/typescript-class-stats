8d8d6785f28d44065e09c56e05c5b4c794b01ca5
Truong Dao <ton.truong.dao@outlook.com>
2021-04-16 12:46:27 -0400

    Converted dateString to Date type

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,9 +1,10 @@
 import fs from 'fs'
 
+import { dateStringtoDate } from './utils'
+
 export class CsvFileReader {
     data: string[][] = [];
     constructor(public filename: string) {};
-
 
     read(): void {
         this.data = fs.readFileSync(this.filename, {
@@ -13,5 +14,21 @@ export class CsvFileReader {
         .map((row: string):string[] => {
             return row.split(',')
         })
+        .map((row: string[]): any => { // Data Conversion
+            return [
+                dateStringtoDate(row[0]),
+                row[1],
+                row[2],
+                parseInt(row[3]),
+                parseInt(row[4]),
+                
+
+            ]
+        })
     }
+
+
+
+
+
 }
